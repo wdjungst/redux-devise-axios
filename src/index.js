05f8e59a7dcb4f088bb3221cfb0f1a89c3a54385
David Jungst <djungst@gmail.com>
2017-09-27 11:35:44 -0600

    add native app AsyncStorage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let HEADERS = ['access-token', 'token-type', 'client', 'expiry', 'uid']
 const tokenMiddleware = args => store => next => action => {
   if (!action)
     action = { type: '' }
-  let { customHeaders = [], validateAction = 'VALIDATE_TOKEN',  logoutAction = 'LOGOUT', axios } = args;
+  let { customHeaders = [], validateAction = 'VALIDATE_TOKEN',  logoutAction = 'LOGOUT', axios, nativeApp } = args;
   HEADERS = [...new Set([...HEADERS, ...customHeaders])]
   if (action.type === validateAction) {
     HEADERS.forEach( token => axios.defaults.headers.common[token] = localStorage.getItem(token) );
@@ -17,7 +17,15 @@ const tokenMiddleware = args => store => next => action => {
       if(headers['access-token']) {
         HEADERS.forEach( token => {
           axios.defaults.headers.common[token] = headers[token];
-          localStorage.setItem(token, headers[token])
+          if (nativeApp) {
+            try {
+              await AsyncStorage.setItem(token, headers[token])
+            } catch (err) {
+              console.log(err)
+            }
+          } else {
+            localStorage.setItem(token, headers[token])
+          }
         })
       }
     }
