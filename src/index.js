ce5bde344fc2c9ffd1adc5ba74c9f2936461f0a7
Harry Plumer <harry.plumer@gmail.com>
2017-10-19 22:10:05 -0600

    added native integration

--- src/index.js
+++ src/index.js
@@ -3,22 +3,34 @@ let HEADERS = ['access-token', 'token-type', 'client', 'expiry', 'uid']
 const tokenMiddleware = args => store => next => action => {
   if (!action)
     action = { type: '' }
-  let { customHeaders = [], validateAction = 'VALIDATE_TOKEN',  logoutAction = 'LOGOUT', axios } = args;
+  let { customHeaders = [], validateAction = 'VALIDATE_TOKEN',  logoutAction = 'LOGOUT', axios, native } = args;
   HEADERS = [...new Set([...HEADERS, ...customHeaders])]
   if (action.type === validateAction) {
-    HEADERS.forEach( token => axios.defaults.headers.common[token] = localStorage.getItem(token) );
+    if (native)
+      HEADERS.forEach( async (token) => axios.defaults.headers.common[token] = await AsyncStorage.getItem(token));
+    else 
+      HEADERS.forEach( token => axios.defaults.headers.common[token] = localStorage.getItem(token));
   } else if (action.type === logoutAction) {
-    HEADERS.forEach( token => {
-      localStorage.removeItem(token);
-    });
+    if (native)
+      HEADERS.forEach( async (token) => await AsyncStorage.removeItem(token));
+    else
+      HEADERS.forEach( token => localStorage.removeItem(token));
   } else {
     let { headers } = action;
     if (headers) {
       if(headers['access-token']) {
-        HEADERS.forEach( token => {
+        if (native){
+          HEADERS.forEach( async (token) => {
+            axios.defaults.headers.common[token] = headers[token];
+              await AsyncStorage.setItem(token, headers[token])
+          })
+        }
+        else {
+          HEADERS.forEach( token => {
           axios.defaults.headers.common[token] = headers[token];
             localStorage.setItem(token, headers[token])
-        })
+          })
+        }
       }
     }
   }
