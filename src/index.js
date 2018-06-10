8d7a48df7aace65a1e1bbe3315a116b1391785a1
David Jungst <djungst@gmail.com>
2018-06-10 11:37:47 -0600

    async for native

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 let HEADERS = ['access-token', 'token-type', 'client', 'expiry', 'uid']
 
+const async setTokens = (headers, axios) => {
+  for (token of headers) {
+    axios.defaults.headers.common[token] = headers[token];
+    await AsyncStorage.setItem(token, headers[token])
+  }
+}
+
+const async getTokens = (headers, axios) => {
+  for (token of headers) {
+    axios.defaults.headers.common[token] = headers[token];
+    await AsyncStorage.getItem(token, headers[token])
+  }
+}
+
+const async clearTokens = (headers) => {
+  for (token of headers) {
+    await AsyncStorage.getItem(token, headers[token])
+  }
+}
+
 const tokenMiddleware = args => store => next => action => {
   if (!action)
     action = { type: '' }
@@ -7,12 +27,12 @@ const tokenMiddleware = args => store => next => action => {
   HEADERS = [...new Set([...HEADERS, ...customHeaders])]
   if (action.type === validateAction) {
     if (native)
-      HEADERS.forEach( async (token) => axios.defaults.headers.common[token] = await AsyncStorage.getItem(token));
+      getTokens(headers, axios);
     else 
       HEADERS.forEach( token => axios.defaults.headers.common[token] = localStorage.getItem(token));
   } else if (action.type === logoutAction) {
     if (native)
-      HEADERS.forEach( async (token) => await AsyncStorage.removeItem(token));
+      clearTokens(headers);
     else
       HEADERS.forEach( token => localStorage.removeItem(token));
   } else {
@@ -20,14 +40,10 @@ const tokenMiddleware = args => store => next => action => {
     if (headers) {
       if(headers['access-token']) {
         if (native){
-          HEADERS.forEach( async (token) => {
-            axios.defaults.headers.common[token] = headers[token];
-              await AsyncStorage.setItem(token, headers[token])
-          })
-        }
-        else {
+          setTokens(headers)
+        } else {
           HEADERS.forEach( token => {
-          axios.defaults.headers.common[token] = headers[token];
+            axios.defaults.headers.common[token] = headers[token];
             localStorage.setItem(token, headers[token])
           })
         }
